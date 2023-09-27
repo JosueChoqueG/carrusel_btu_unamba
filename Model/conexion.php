1e7038ecb1cc7ac5b6cd34ca200400a6fa4fa10e
Josue Choque Gomez <110512792+JosueChoqueG@users.noreply.github.com>
2023-09-27 12:29:12 -0500

    cambio ruta DB

--- Model/conexion.php
+++ Model/conexion.php
@@ -1,4 +1,5 @@
-<?php 
-		$conexion = mysqli_connect("localhost", "root", "", "bolsadet_job_boart");	
-	
- ?>
+
+ <?php 
+    $conn = mysqli_connect("167.114.141.141", "bolsadet_user", "BMm0fmzu4nUq", "bolsadet_job_boart"); 
+    
+?>
